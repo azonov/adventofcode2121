fee7063493181668b72a3adf40b0188543bb8762
Zonov Andrey Vladimirovich <ext.azonov@tinkoff.ru>
2021-12-07 13:49:42 +0300

    Simplify

--- day7/main.swift
+++ day7/main.swift
@@ -1,49 +1,24 @@
 import Foundation
 
 let coordinates = [16,1,2,0,4,2,7,1,2,14]
-let minC = coordinates.min()!
-let maxC = coordinates.max()!
+let minCoordinate = coordinates.min()!
+let maxCoordinate = coordinates.max()!
+
+func minFuel(valueCalculator: (Int) -> Int) -> Int
+{
+    (minCoordinate...maxCoordinate)
+        .map { value in
+            coordinates.reduce(0, { $0 + valueCalculator(abs(value - $1)) })
+        }
+        .min()!
+}
 
 func part1() -> Int {
-    var ans = Int.max
-    for i in minC..<maxC {
-        var fuel = 0
-        for c in coordinates {
-            fuel += max(c, i) -  min(c, i)
-            if fuel > ans {
-                break
-            }
-        }
-        if fuel < ans {
-            ans = fuel
-        }
-    }
-    return ans
+    minFuel(valueCalculator: { $0 })
 }
 
 func part2() -> Int {
-    var ans = Int.max
-    var triang: [Int: Int] = [:]
-    var value = 0
-    triang[0] = 0
-    for i in 1...maxC-minC {
-        value += i
-        triang[i] = value
-    }
-    for i in minC..<maxC {
-        var fuel = 0
-        for c in coordinates {
-            let dif = max(c, i) -  min(c, i)
-            fuel += triang[dif]!
-            if fuel > ans {
-                break
-            }
-        }
-        if fuel < ans {
-            ans = fuel
-        }
-    }
-    return ans
+    minFuel { $0 * ($0 + 1) / 2 }
 }
 
 print(part1())
